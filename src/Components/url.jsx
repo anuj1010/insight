32457639acf05fae3856e5915f97956f02d9dfdc
Anuj Singh <editingwithanuj@gmail.com>
2024-02-25 10:58:46 +0530

    url

--- src/Components/url.jsx
+++ src/Components/url.jsx
@@ -1,4 +1,4 @@
-// const baseUrl = "https://long-plum-marlin-hat.cyclic.app/api/blog/";
-const baseUrl = "http://localhost:5000/api/blog/";
+const baseUrl = "https://long-plum-marlin-hat.cyclic.app/api/blog/";
+// const baseUrl = "http://localhost:5000/api/blog/";
 
 export default baseUrl;
